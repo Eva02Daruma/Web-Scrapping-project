25e36275230c83a8640f861c07af9f208db713f6
Jose araya <jose.arayaprohens@gmail.com>
2022-06-07 16:29:22 -0400

    Primer tutorial de tech with tim terminado

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from torch import parse_type_comment
 
 url = "https://www.zmart.cl/scripts/prodView.asp?idProduct=81279"
 
@@ -7,5 +8,12 @@ result = requests.get(url)
 doc = BeautifulSoup(result.text , "html.parser")
 
 
-precios = doc.find_all(text = "$")
+precios = doc.find_all( id = "PriceProduct")
+parent = precios[0].parent
 
+span = parent.find_all("span" , class_="ProdBox146_Precio")
+div = parent.find("div")
+for res in div:
+    print(res.text)
+
+#print(parent)
